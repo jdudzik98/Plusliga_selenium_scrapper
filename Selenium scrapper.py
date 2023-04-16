de0eddf303b69eaabe21a9e82d1191b4475d1d73
jandudzik <dudzikjanek@gmail.com>
2023-04-16 15:40:10 +0200

    Created whole match table and saved whole match data into csv file

--- Selenium scrapper.py
+++ Selenium scrapper.py
@@ -21,7 +21,7 @@ try:
 except NoSuchElementException:
     print('not clicked')
     pass
-time.sleep(10)
+
 
 # Fetch match information:
 try:
@@ -87,23 +87,63 @@ except NoSuchElementException:
     players = None
     print("Couldn't find players")
 
+#create a dictionary with the match data:
+single_match_info = {'Date': Date,  # Date of the match
+                'Phase': Phase,  # Phase of the match
+                'Spectators': Spectators,  # Number of spectators
+                'Capacity': Capacity,  # Capacity of the hall
+                'Team1': unique_href_list[0],  # Team 1
+                'Team2': unique_href_list[1],  # Team 2
+                'Result_1': result[0],  # Result of the match
+                'Result_2': result[1],  # Result of the match
+                #'Players': players
+                }
+
 # Create a dataframe:
-df = pd.DataFrame({'Date': Date,  # Date of the match
-                   'Phase': Phase,  # Phase of the match
-                   'Spectators': Spectators,  # Number of spectators
-                   'Capacity': Capacity,  # Capacity of the hall
-                   'Team1': unique_href_list[0],  # Team 1
-                   'Team2': unique_href_list[1],  # Team 2
-                   'Result_1': result[0],  # Result of the match
-                   'Result_2': result[1]},  # Result of the match
-                  index=[0])
+match_dataframe = pd.DataFrame({'Date': [],  # Date of the match
+                                'Phase': [],  # Phase of the match
+                                'Spectators': [],  # Number of spectators
+                                'Capacity': [],  # Capacity of the hall
+                                'Team1': [],  # Team 1
+                                'Team2': [],  # Team 2
+                                'Result_1': [],  # Result of the match
+                                'Result_2': [],  # Result of the match
+                                'Players': [],
+                                'set_score_host': [],
+                                'set_score_guest': [],
+                                'point_score_host': [],
+                                'point_score_guest': [],
+                                'set_score_host_before': [],
+                                'set_score_guest_before': [],
+                                'host_score': [],
+                                'guest_score': [],
+                                'serving_team': [],
+                                'serving_player': [],
+                                'serve_result': [],
+                                'serve_effect': [],
+                                'receiver': [],
+                                'receive_skill': [],
+                                'receive_effect': [],
+                                'net_crossings': [],
+                                'point_winner': [],
+                                'Timeouts_host': [],
+                                'Timeouts_guest': [],
+                                'Challenges_host': [],
+                                'Challenges_guest': []})
+
 
 # Iterate over points:
 driver.switch_to.frame(0)
 sets = driver.find_element(By.XPATH, "//div[@class='play-by-play-container']")\
     .find_elements(By.XPATH, ".//div[@class='events-container']")
 
-for set in sets[0:1]:
+for set in sets:
+    # Store local variables:
+    Timeouts_host = 0
+    Timeouts_guest = 0
+    Challenges_host = 0
+    Challenges_guest = 0
+
 
     # Fetch set information:
     set_info = set.find_element(By.XPATH, ".//vsw-end-set-play-by-play[@class='w100']")
@@ -131,6 +171,16 @@ for set in sets[0:1]:
         find_elements(By.XPATH, ".//span[@class='player-nr']")
     print("guest team: ", [player.text for player in guest_team])
 
+    # create a dictionary with the set information to append to match dataframe
+    single_set_info = {'set_score_host': set_score_host,
+                       'set_score_guest': set_score_guest,
+                       'point_score_host': point_score_host,
+                       'point_score_guest': point_score_guest,
+                       'set_score_host_before': set_score_host_before,
+                       'set_score_guest_before': set_score_guest_before}
+
+
+
     # Fetch points:
     play_by_play = set.find_elements(By.XPATH, ".//div[@class='w100']")
     for point in reversed(play_by_play):
@@ -140,6 +190,14 @@ for set in sets[0:1]:
             serving_player = point.find_element(By.XPATH, ".//p[@class='shirt-number']").text
             serve_result = point.find_element(By.XPATH, ".//span[@class='skill']").text
             serve_effect = point.find_element(By.XPATH, ".//span[@class='effect']").text
+
+            # Check which team scored the point
+            try:
+                point_winner = point.find_element(By.XPATH, ".//div[@class = 'rally-play-by-play event-play-by-play right']")
+                point_winner = "Guest"
+            except NoSuchElementException:
+                point_winner = "Host"
+
             try:  # If first element of the point contains character "right" in class name (i.e. guest is serving):
                 serving = point.find_element(By.XPATH, ".//div[contains(@class, 'plays-play-by-play')]/*[1]").\
                     find_element(By.XPATH, ".//div[@class='rally-playrow-play-by-play right']").text
@@ -159,22 +217,103 @@ for set in sets[0:1]:
                     find_element(By.XPATH, ".//span[@class='skill']").text
                 receive_effect = point.find_element(By.XPATH, ".//div[contains(@class, 'plays-play-by-play')]/*[2]").\
                     find_element(By.XPATH, ".//span[@class='effect']").text
+
             except NoSuchElementException:
                 receiver = None
                 receive_skill = None
                 receive_effect = None
 
+            try:
+                touches = point.find_elements(By.XPATH,
+                                               './/div[contains(@class, "rally-playrow-play-by-play") or contains(@class, "rally-playrow-play-by-play right")]')
+                net_crossings = 0
+                current_touch = serving
+                last_touch = serving
+
+                for touch in touches:
+                    class_name = touch.get_attribute("class")
+                    if "right" in class_name:
+                        current_touch = "Guest"
+                    else:
+                        current_touch = "Host"
+                    if current_touch != last_touch:
+                        net_crossings += 1
+                    last_touch = current_touch
+                if touch.find_element(By.XPATH, ".//span[@class='skill']").text in ["Attack", "Block"] and \
+                        touch.find_element(By.XPATH, ".//span[@class='effect']").text != "error":
+                    net_crossings += 1
+
+            except NoSuchElementException:
+                print( "No right class")
+
+
+
             print(host_score, ":", guest_score, " serving player: ", serving_player, " from team ", serving,
-                  " with effect", serve_result,  serve_effect, "\n"
-                  "receiver: ", receiver, " with effect", receive_skill, receive_effect, "\n")
+                  " with effect", serve_result, serve_effect, "\n", "receiver: ", receiver, " with effect",
+                  receive_skill, receive_effect, "\n", point_winner, " scored with ", net_crossings, " net crossings\n")
+
+            # create a dictionary with the point information to append to set dataframe
+            single_point_info = {'host_score': host_score,
+                                 'guest_score': guest_score,
+                                 'serving_player': serving_player,
+                                 'serving_team': serving,
+                                 'serve_result': serve_result,
+                                 'serve_effect': serve_effect,
+                                 'receiver': receiver,
+                                 'receive_skill': receive_skill,
+                                 'receive_effect': receive_effect,
+                                 'net_crossings': net_crossings,
+                                 'point_winner': point_winner,
+                                 'Timeouts_host': Timeouts_host,
+                                 'Timeouts_guest': Timeouts_guest,
+                                 'Challenges_host': Challenges_host,
+                                 'Challenges_guest': Challenges_guest}
+            single_point_info = dict(single_point_info, **single_set_info, **single_match_info)
+            print(single_point_info)
+            match_dataframe.loc[len(match_dataframe)] = single_point_info
 
         except NoSuchElementException:
-            print("No score")
-            continue
+            try:
+                challenge = point.find_element(By.XPATH,
+                                               ".//div[contains(@class, 'video-challenge-play-by-play')]")
+                try:
+                    challenge = point.find_element(By.XPATH,
+                                                   ".//div[@class='video-challenge-play-by-play event-play-by-play right']")
+                    print("Challenge right")
+                    Challenges_guest += 1
+                except NoSuchElementException:
+                    print("Challenge left")
+                    Challenges_host += 1
+
+            except NoSuchElementException:
+                try:
+                    timeout = point.find_element(By.XPATH, ".//div[contains(@class, 'timeout-play-by-play')]")
+                    try:
+                        timeout = point.find_element(By.XPATH,
+                                                 ".//div[@class='timeout-play-by-play event-play-by-play right']")
+                        print("Timeout right")
+                        Timeouts_guest += 1
+                    except NoSuchElementException:
+                        print("Timeout left")
+                        Timeouts_host += 1
+                except NoSuchElementException:
+                    print("No score")
+
     print("\n\n\n")
 
+# TODO:
+# 1. create frames for each point            Done
+# 2. create frames for each set              Done
+# 3. create frames for each match            Done
+# 4. Handle "No score" exception -> subs
+# 5. Handle timeouts                         Done
+# 6. Create touches dict
+# 7. Create table position
+# 8. Maybe create recent form?
+# 9. Games crawler
 
-df.to_csv('Matches.csv', index=False)
+
+match_dataframe.to_csv('Matches.csv', index=False)
 
 # Close browser:
 driver.quit()
