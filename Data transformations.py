6848a593e5b8e8526c114bf7813ec0a11b115feb
jandudzik <dudzikjanek@gmail.com>
2023-04-18 12:27:25 +0200

    Cleaning dataset

--- Data transformations.py
+++ Data transformations.py
@@ -3,11 +3,11 @@ import pandas as pd
 # read CSV file as a dataframe
 matches = pd.read_csv('Matches.csv')
 table = pd.read_csv('Table_standings.csv')
+table_host = table.rename(columns={c: c+'_table_host' for c in table.columns if c not in ['Year', 'Round', 'Team']})
+table_guest = table.rename(columns={c: c+'_table_guest' for c in table.columns if c not in ['Year', 'Round', 'Team']})
 
-#merged_df = pd.merge(matches, table, on=['Year', 'Round'], how='outer')
-#merged_df = pd.merge(matches, table, on=['Year', 'Round', ], how='outer', suffixes=('_df1', '_df2'))
-merged_df = pd.merge(matches, table, left_on=['Year', 'Round', 'Team1_href'], right_on=['Year', 'Round', 'Team'], how='outer', suffixes=('', '_table_host'))
-merged_df = pd.merge(merged_df, table, left_on=['Year', 'Round', 'Team2_href'], right_on=['Year', 'Round', 'Team'], how='outer', suffixes=('', '_table_guest'))
+merged_df = pd.merge(matches, table_host, left_on=['Year', 'Round', 'Team1_href'], right_on=['Year', 'Round', 'Team'], how='outer', validate = "m:1")
+merged_df = pd.merge(merged_df, table_guest, left_on=['Year', 'Round', 'Team2_href'], right_on=['Year', 'Round', 'Team'], how='outer', validate = "m:1")
 
 
 merged_df.to_csv('Plusliga_data.csv', index=False)
