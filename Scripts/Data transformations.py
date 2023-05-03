32f5d1cf6939dadf906ff19ddd3cac74d41c8916
jandudzik <dudzikjanek@gmail.com>
2023-05-03 13:09:57 +0200

    Restructuring the repo

---
+++ Scripts/Data transformations.py
@@ -0,0 +1,117 @@
+import pandas as pd
+import numpy as np
+
+# read CSV file as a dataframe
+matches = pd.read_csv('Matches_batch2 copy 2.csv')
+table = pd.read_csv('Table_standings.csv')
+
+table_host = table.rename(columns={c: c + '_table_host' for c in table.columns if c not in ['Year', 'Round', 'Team']})
+table_guest = table.rename(columns={c: c + '_table_guest' for c in table.columns if c not in ['Year', 'Round', 'Team']})
+
+merged_df = pd.merge(matches, table_host, left_on=['Year', 'Round', 'Team1_href'],
+                     right_on=['Year', 'Round', 'Team'], how='left', validate="m:1")
+merged_df = pd.merge(merged_df, table_guest, left_on=['Year', 'Round', 'Team2_href'],
+                     right_on=['Year', 'Round', 'Team'], how='left', validate="m:1")
+
+# Remove the redundant columns:
+merged_df = merged_df.drop(columns=['Team_x', 'Team_y'])
+
+# Extract the hour and day of week from the date:
+merged_df['Date'] = pd.to_datetime(merged_df['Date'], dayfirst=True)
+merged_df['Hour'] = merged_df['Date'].dt.strftime('%H:%M')
+merged_df['Day_of_week'] = merged_df['Date'].dt.day_name()
+
+# Calculate the current point and set difference:
+merged_df['Current_point_difference'] = merged_df['Current_point_score_host'] - merged_df['Current_point_score_guest']
+merged_df['Current_set_difference'] = merged_df['Before_set_score_host'] - merged_df['Before_set_score_guest']
+
+# calculate running max and min points difference partitioned by match and set
+merged_df['Max_point_difference_throughout_set'] = merged_df.groupby(['MatchID', 'Set_number'])[
+    'Current_point_difference'].rolling(window=100, min_periods=1).max().values
+merged_df['Min_point_difference_throughout_set'] = merged_df.groupby(['MatchID', 'Set_number'])[
+    'Current_point_difference'].rolling(window=100, min_periods=1).min().values
+merged_df['Max_point_difference_throughout_match'] = merged_df.groupby(['MatchID'])['Current_point_difference'].rolling(
+    window=10000, min_periods=1).max().values
+merged_df['Min_point_difference_throughout_match'] = merged_df.groupby(['MatchID'])['Current_point_difference'].rolling(
+    window=10000, min_periods=1).min().values
+
+# Calculate the running average of the net crossings:
+merged_df['Running_net_crossings_average'] = merged_df.groupby(['MatchID'])['Net_crossings_number'].rolling(
+    window=1000, min_periods=1).mean().values
+
+# Calculate the sum of receives for each team:
+merged_df['Current_host_receives'] = merged_df[[col for col in merged_df.columns if
+                                                col.startswith('Current_host_receive')]].sum(axis=1)
+merged_df['Current_guest_receives'] = merged_df[[col for col in merged_df.columns if
+                                                col.startswith('Current_guest_receive')]].sum(axis=1)
+
+# Calculate the form stats - serve effectiveness, positive_reception_ratio, perfect reception ratio, attack accuracy, attack effectiveness:
+merged_df['Current_host_serve_effectiveness'] = (merged_df['Current_host_serve_aces'] - merged_df['Current_host_serve_errors'])/merged_df['Current_host_serves']
+merged_df['Current_guest_serve_effectiveness'] = (merged_df['Current_guest_serve_aces'] - merged_df['Current_guest_serve_errors'])/merged_df['Current_guest_serves']
+merged_df['Current_host_positive_reception_ratio'] = (merged_df['Current_host_receive_positive'] + merged_df['Current_host_receive_perfect'])/merged_df['Current_host_receives']
+merged_df['Current_guest_positive_reception_ratio'] = (merged_df['Current_guest_receive_positive'] + merged_df['Current_guest_receive_perfect'])/merged_df['Current_guest_receives']
+merged_df['Current_host_perfect_reception_ratio'] = merged_df['Current_host_receive_perfect']/merged_df['Current_host_receives']
+merged_df['Current_guest_perfect_reception_ratio'] = merged_df['Current_guest_receive_perfect']/merged_df['Current_guest_receives']
+merged_df['Current_host_negative_reception_ratio'] = (merged_df['Current_host_receive_negative']+merged_df['Current_host_receive_ball_returned'] + merged_df['Current_host_receive_errors'])/merged_df['Current_host_receives']
+merged_df['Current_guest_negative_reception_ratio'] = (merged_df['Current_guest_receive_negative']+merged_df['Current_guest_receive_ball_returned'] + merged_df['Current_guest_receive_errors'])/merged_df['Current_guest_receives']
+merged_df['Current_host_attack_accuracy'] = merged_df['Current_host_attacks_scored']/merged_df['Current_host_attacks']
+merged_df['Current_guest_attack_accuracy'] = merged_df['Current_guest_attacks_scored']/merged_df['Current_guest_attacks']
+merged_df['Current_host_attack_effectiveness'] = (merged_df['Current_host_attacks_scored']-merged_df['Current_host_attack_errors']-merged_df['Current_host_attacks_blocked'])/merged_df['Current_guest_attacks']
+merged_df['Current_guest_attack_effectiveness'] = (merged_df['Current_guest_attacks_scored']-merged_df['Current_guest_attack_errors']-merged_df['Current_guest_attacks_blocked'])/merged_df['Current_host_attacks']
+
+# Fill NaNs in columns from 90th to the end with 0:
+merged_df.iloc[:, 90:] = merged_df.iloc[:, 90:].fillna(0)
+
+# replace inf and -inf with 0
+merged_df = merged_df.replace([np.inf, -np.inf], 0)
+
+# create a Winner column with True if the maximum value is 3, and False otherwise
+merged_df['Winner'] = merged_df.groupby(['MatchID'])['Current_set_score_host'].transform(max) == 3
+
+# create a relative capacity column:
+merged_df['Relative_capacity'] = merged_df['Spectators']/merged_df['Capacity']
+
+# Add previous matches performance for last 5 matches:
+# Decrease the Round number by 5 if it is greater than 5. Decrease it to 1 in other cases:
+merged_df['Round_prev'] = np.where(merged_df['Round'] > 5, merged_df['Round'] - 5, 1)
+
+# Rename columns for old table:
+table_host = table.rename(columns={c: c + '_table_host_prev' for c in table.columns if c not in ['Year', 'Round', 'Team']})
+table_guest = table.rename(columns={c: c + '_table_guest_prev' for c in table.columns if c not in ['Year', 'Round', 'Team']})
+
+# Merge the table with itself to get the previous performance:
+merged_df = pd.merge(merged_df, table_host, left_on=['Year', 'Round_prev', 'Team1_href'],
+                     right_on=['Year', 'Round', 'Team'], how='left', validate="m:1")
+merged_df = pd.merge(merged_df, table_guest, left_on=['Year', 'Round_prev', 'Team2_href'],
+                     right_on=['Year', 'Round', 'Team'], how='left', validate="m:1")
+
+# Creating new columns for the difference between the current and previous performance:
+merged_df['Matches_ratio_last_5_host'] = merged_df['Won_matches_table_host'] - \
+                                         merged_df['Won_matches_table_host_prev']- \
+                                         merged_df['Lost_matches_table_host'] + \
+                                         merged_df['Lost_matches_table_host_prev']
+merged_df['Matches_ratio_last_5_guest'] = merged_df['Won_matches_table_guest'] - \
+                                          merged_df['Won_matches_table_guest_prev']- \
+                                          merged_df['Lost_matches_table_guest']+ \
+                                          merged_df['Lost_matches_table_guest_prev']
+
+# Removing unnecessary columns created during the merge:
+merged_df = merged_df[merged_df.columns.drop(list(merged_df.filter(regex='_prev')))]
+merged_df = merged_df.drop(['Team_y', 'Team_x', 'Round', 'Round_y'], axis=1)
+merged_df = merged_df.rename(columns={'Round_x': 'Round'})
+
+# Removing unnecessary columns from the dataframe:
+merged_df = merged_df.drop(['Date', 'Capacity', 'Current_set_score_host', 'Current_set_score_guest',
+                            'Final_point_score_host', 'Final_point_score_guest'], axis=1)
+
+# Save the dataframe to a csv file:
+merged_df.to_csv('Plusliga_data.csv', index=False)
+
+# TODO:
+# 1. Add the last 5 matches results for each team DONE
+# 2. Add the recent form - last 5 receptions, last 5 serves, last 5 points - switched to running sum
+# 3. Add the maximal point difference in the set and match      DONE
+# 4. When the phase == playoff, change it to last round of the regular season DONE
+# 5. Review the variables and change them to running sums/averages etc. DONE
+# 6. Add the historical matches between two teams
+
---
+++ Scripts/Table_standings_scrapper.py
@@ -0,0 +1,134 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
+import pandas as pd
+from tqdm import tqdm
+
+driver = webdriver.Chrome('./chromedriver')
+
+# Create the dataframe to store the data:
+df = pd.DataFrame({'Year': [],
+                   'Round': [],
+                   'Current_position': [],
+                   'Team': [],
+                   'Season_points': [],
+                   'Played_matches': [],
+                   'Won_matches': [],
+                   'Lost_matches': [],
+                   'Sets_won': [],
+                   'Sets_lost': [],
+                   'Points_won': [],
+                   'Points_lost': [],
+                   'Sets_ratio': [],
+                   'Points_ratio': []}
+                  )
+
+# create loop to create urls for each season
+for year in tqdm(range(2020, 2023)):
+    url = 'https://www.plusliga.pl/table/tour/' + str(year) + '.html'
+    driver.get(url)
+    # Click the cookies button:
+    try:
+        wait = WebDriverWait(driver, 10)
+        element = wait.until(
+            ec.element_to_be_clickable((By.XPATH, "/html/body/footer/div[6]/div/div[4]/div/button[4]")))
+        element.click()
+
+        print('clicked cookies')
+    except NoSuchElementException:
+        print('not clicked cookies')
+        pass
+    except TimeoutException:
+        pass
+
+    # Click the phase button:
+    try:
+        wait = WebDriverWait(driver, 10)
+        element = wait.until(ec.element_to_be_clickable((By.XPATH, "//*[@data-name='1_sts_rs']")))
+        element.click()
+
+        print('clicked phase')
+    except NoSuchElementException:
+        print('not clicked phase')
+        pass
+    except TimeoutException:
+        pass
+    # Get the round buttons:
+    round_buttons = driver.find_element(By.XPATH,
+                                        "//div[contains(@class, "
+                                        "'col-xs-12 bar sort filtr fazy rundy kolejki grupa-1 faza-1')]"). \
+        find_elements(By.XPATH, ".//a")
+    for round_button in tqdm(round_buttons[1:len(round_buttons)]):
+        # Click the round button:
+        try:
+            round_button.click()
+            # print('clicked round')
+
+            # Get the table:
+            table = driver.find_element(By.XPATH, "//table[@class='rs-standings-table table table-bordered "
+                                                  "table-hover table-condensed']")
+            # Get the table rows:
+            rows = table.find_elements(By.XPATH, ".//tbody/tr[not(contains(@class, 'hidden'))]")
+
+            for row in rows:
+                # get the rows attributes:
+                round_number = row.get_attribute('data-termin')[4:]
+                team = row.find_element(By.XPATH, ".//td[2]/a").get_attribute('href')
+                point_info = row.find_elements(By.XPATH, ".//td")
+
+                # Create a dictionary:
+                data = {'Year': year,
+                        'Round': round_number,
+                        'Current_position': point_info[0].text,
+                        'Team': team,
+                        'Season_points': point_info[2].text,
+                        'Played_matches': point_info[3].text,
+                        'Won_matches': point_info[4].text,
+                        'Lost_matches': point_info[5].text,
+                        'Sets_won': point_info[6].text,
+                        'Sets_lost': point_info[7].text,
+                        'Points_won': point_info[8].text,
+                        'Points_lost': point_info[9].text,
+                        'Sets_ratio': point_info[10].text,
+                        'Points_ratio': point_info[11].text,
+                        '3-0 wins': point_info[12].text,
+                        '3-1 wins': point_info[13].text,
+                        '3-2 wins': point_info[14].text,
+                        '2-3 losses': point_info[15].text,
+                        '1-3 losses': point_info[16].text,
+                        '0-3 losses': point_info[17].text}
+
+                # Append the dictionary to the dataframe:
+                df.loc[len(df)] = data
+        except NoSuchElementException:
+            print('not clicked round')
+            pass
+
+# Save the dataframe to a csv file:
+df.to_csv('table_standings_old.csv', index=False)
+# Add rows to handle play-off phase:
+new_df = pd.DataFrame(columns=df.columns)
+# Loop over each year in the DataFrame
+for year in df['Year'].unique():
+    # Get the maximum round for the year
+    max_round = df.loc[df['Year'] == year, 'Round'].max()
+
+    # Append the rows with the maximum round to the new DataFrame
+    new_df = pd.concat([new_df, df.loc[(df['Year'] == year) & (df['Round'] == max_round)]])
+
+    # Duplicate the rows with the maximum round and change the Round value to NaN
+    max_round_rows = df.loc[(df['Year'] == year) & (df['Round'] == max_round)].copy()
+    max_round_rows['Round'] = None
+
+    # Append the duplicated rows to the new DataFrame
+    new_df = pd.concat([new_df, max_round_rows])
+
+# Combine the new DataFrame with the original DataFrame and remove duplicates
+new_df = pd.concat([df, new_df]).drop_duplicates(keep=False)
+
+# Save the dataframe to a csv file:
+new_df.to_csv('table_standings.csv', index=False)
+# Close browser:
+driver.quit()
