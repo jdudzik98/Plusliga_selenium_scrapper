ca92d945d23482699e19f6bc0fd86281d9addc2d
jandudzik <dudzikjanek@gmail.com>
2023-05-04 20:17:47 +0200

    Prepared Data transformations.py file for modelling

--- Scripts/Data transformations.py
+++ Scripts/Data transformations.py
@@ -2,8 +2,20 @@ import pandas as pd
 import numpy as np
 
 # read CSV file as a dataframe
-matches = pd.read_csv('Matches_batch2 copy 2.csv')
-table = pd.read_csv('Table_standings.csv')
+batch1 = pd.read_csv('../Datasets/Matches_batch1.csv')
+batch2 = pd.read_csv('../Datasets/Matches_batch2.csv')
+batch3 = pd.read_csv('../Datasets/Matches_batch3.csv')
+batch4 = pd.read_csv('../Datasets/Matches_batch4.csv')
+batch5 = pd.read_csv('../Datasets/Matches_batch5.csv')
+batch6 = pd.read_csv('../Datasets/Matches_batch6.csv')
+
+# concatenate the dataframes
+matches = pd.concat([batch1, batch2, batch3, batch4, batch5, batch6], ignore_index=True)
+table = pd.read_csv('../Datasets/Table_standings.csv')
+
+# Duplicate the 'Round' column:
+matches['Round_original'] = matches['Round']
+matches['Round'] = matches['Round'] - 1
 
 table_host = table.rename(columns={c: c + '_table_host' for c in table.columns if c not in ['Year', 'Round', 'Team']})
 table_guest = table.rename(columns={c: c + '_table_guest' for c in table.columns if c not in ['Year', 'Round', 'Team']})
@@ -20,6 +32,16 @@ merged_df = merged_df.drop(columns=['Team_x', 'Team_y'])
 merged_df['Date'] = pd.to_datetime(merged_df['Date'], dayfirst=True)
 merged_df['Hour'] = merged_df['Date'].dt.strftime('%H:%M')
 merged_df['Day_of_week'] = merged_df['Date'].dt.day_name()
+
+# Map hour and day of week to new categories
+merged_df.loc[(merged_df['Day_of_week'].isin(['Saturday', 'Sunday'])) &
+              (merged_df['Hour'] < '16:00'), 'Time_Category'] = 'weekend afternoon'
+merged_df.loc[(merged_df['Day_of_week'].isin(['Saturday', 'Sunday'])) &
+              (merged_df['Hour'] >= '16:00'), 'Time_Category'] = 'weekend evening'
+merged_df.loc[(~merged_df['Day_of_week'].isin(['Saturday', 'Sunday'])) &
+              (merged_df['Hour'] < '19:00'), 'Time_Category'] = 'workday evening'
+merged_df.loc[(~merged_df['Day_of_week'].isin(['Saturday', 'Sunday'])) &
+              (merged_df['Hour'] >= '19:00'), 'Time_Category'] = 'workday late evening'
 
 # Calculate the current point and set difference:
 merged_df['Current_point_difference'] = merged_df['Current_point_score_host'] - merged_df['Current_point_score_guest']
@@ -43,21 +65,35 @@ merged_df['Running_net_crossings_average'] = merged_df.groupby(['MatchID'])['Net
 merged_df['Current_host_receives'] = merged_df[[col for col in merged_df.columns if
                                                 col.startswith('Current_host_receive')]].sum(axis=1)
 merged_df['Current_guest_receives'] = merged_df[[col for col in merged_df.columns if
-                                                col.startswith('Current_guest_receive')]].sum(axis=1)
+                                                 col.startswith('Current_guest_receive')]].sum(axis=1)
 
-# Calculate the form stats - serve effectiveness, positive_reception_ratio, perfect reception ratio, attack accuracy, attack effectiveness:
-merged_df['Current_host_serve_effectiveness'] = (merged_df['Current_host_serve_aces'] - merged_df['Current_host_serve_errors'])/merged_df['Current_host_serves']
-merged_df['Current_guest_serve_effectiveness'] = (merged_df['Current_guest_serve_aces'] - merged_df['Current_guest_serve_errors'])/merged_df['Current_guest_serves']
-merged_df['Current_host_positive_reception_ratio'] = (merged_df['Current_host_receive_positive'] + merged_df['Current_host_receive_perfect'])/merged_df['Current_host_receives']
-merged_df['Current_guest_positive_reception_ratio'] = (merged_df['Current_guest_receive_positive'] + merged_df['Current_guest_receive_perfect'])/merged_df['Current_guest_receives']
-merged_df['Current_host_perfect_reception_ratio'] = merged_df['Current_host_receive_perfect']/merged_df['Current_host_receives']
-merged_df['Current_guest_perfect_reception_ratio'] = merged_df['Current_guest_receive_perfect']/merged_df['Current_guest_receives']
-merged_df['Current_host_negative_reception_ratio'] = (merged_df['Current_host_receive_negative']+merged_df['Current_host_receive_ball_returned'] + merged_df['Current_host_receive_errors'])/merged_df['Current_host_receives']
-merged_df['Current_guest_negative_reception_ratio'] = (merged_df['Current_guest_receive_negative']+merged_df['Current_guest_receive_ball_returned'] + merged_df['Current_guest_receive_errors'])/merged_df['Current_guest_receives']
-merged_df['Current_host_attack_accuracy'] = merged_df['Current_host_attacks_scored']/merged_df['Current_host_attacks']
-merged_df['Current_guest_attack_accuracy'] = merged_df['Current_guest_attacks_scored']/merged_df['Current_guest_attacks']
-merged_df['Current_host_attack_effectiveness'] = (merged_df['Current_host_attacks_scored']-merged_df['Current_host_attack_errors']-merged_df['Current_host_attacks_blocked'])/merged_df['Current_guest_attacks']
-merged_df['Current_guest_attack_effectiveness'] = (merged_df['Current_guest_attacks_scored']-merged_df['Current_guest_attack_errors']-merged_df['Current_guest_attacks_blocked'])/merged_df['Current_host_attacks']
+# Calculate the form stats - serve effectiveness, positive_reception_ratio, perfect reception ratio, attack accuracy,
+# attack effectiveness:
+merged_df['Current_host_serve_effectiveness'] = (merged_df['Current_host_serve_aces'] - merged_df[
+    'Current_host_serve_errors']) / merged_df['Current_host_serves']
+merged_df['Current_guest_serve_effectiveness'] = (merged_df['Current_guest_serve_aces'] - merged_df[
+    'Current_guest_serve_errors']) / merged_df['Current_guest_serves']
+merged_df['Current_host_positive_reception_ratio'] = (merged_df['Current_host_receive_positive'] + merged_df[
+    'Current_host_receive_perfect']) / merged_df['Current_host_receives']
+merged_df['Current_guest_positive_reception_ratio'] = (merged_df['Current_guest_receive_positive'] + merged_df[
+    'Current_guest_receive_perfect']) / merged_df['Current_guest_receives']
+merged_df['Current_host_perfect_reception_ratio'] = merged_df['Current_host_receive_perfect'] / merged_df[
+    'Current_host_receives']
+merged_df['Current_guest_perfect_reception_ratio'] = merged_df['Current_guest_receive_perfect'] / merged_df[
+    'Current_guest_receives']
+merged_df['Current_host_negative_reception_ratio'] = (merged_df['Current_host_receive_negative'] + merged_df[
+    'Current_host_receive_ball_returned'] + merged_df['Current_host_receive_errors']) / merged_df[
+                                                         'Current_host_receives']
+merged_df['Current_guest_negative_reception_ratio'] = (merged_df['Current_guest_receive_negative'] + merged_df[
+    'Current_guest_receive_ball_returned'] + merged_df['Current_guest_receive_errors']) / merged_df[
+                                                          'Current_guest_receives']
+merged_df['Current_host_attack_accuracy'] = merged_df['Current_host_attacks_scored'] / merged_df['Current_host_attacks']
+merged_df['Current_guest_attack_accuracy'] = merged_df['Current_guest_attacks_scored'] / merged_df[
+    'Current_guest_attacks']
+merged_df['Current_host_attack_effectiveness'] = (merged_df['Current_host_attacks_scored'] - merged_df[
+    'Current_host_attack_errors'] - merged_df['Current_host_attacks_blocked']) / merged_df['Current_guest_attacks']
+merged_df['Current_guest_attack_effectiveness'] = (merged_df['Current_guest_attacks_scored'] - merged_df[
+    'Current_guest_attack_errors'] - merged_df['Current_guest_attacks_blocked']) / merged_df['Current_host_attacks']
 
 # Fill NaNs in columns from 90th to the end with 0:
 merged_df.iloc[:, 90:] = merged_df.iloc[:, 90:].fillna(0)
@@ -69,15 +105,18 @@ merged_df = merged_df.replace([np.inf, -np.inf], 0)
 merged_df['Winner'] = merged_df.groupby(['MatchID'])['Current_set_score_host'].transform(max) == 3
 
 # create a relative capacity column:
-merged_df['Relative_capacity'] = merged_df['Spectators']/merged_df['Capacity']
+merged_df['Relative_spectators'] = merged_df['Spectators'] / merged_df['Capacity']
 
 # Add previous matches performance for last 5 matches:
 # Decrease the Round number by 5 if it is greater than 5. Decrease it to 1 in other cases:
-merged_df['Round_prev'] = np.where(merged_df['Round'] > 5, merged_df['Round'] - 5, 1)
+merged_df['Round_prev'] = np.where(merged_df['Round'].isna(), np.nan,
+                                   np.where(merged_df['Round'] > 5, merged_df['Round'] - 5, 1))
 
 # Rename columns for old table:
-table_host = table.rename(columns={c: c + '_table_host_prev' for c in table.columns if c not in ['Year', 'Round', 'Team']})
-table_guest = table.rename(columns={c: c + '_table_guest_prev' for c in table.columns if c not in ['Year', 'Round', 'Team']})
+table_host = table.rename(
+    columns={c: c + '_table_host_prev' for c in table.columns if c not in ['Year', 'Round', 'Team']})
+table_guest = table.rename(
+    columns={c: c + '_table_guest_prev' for c in table.columns if c not in ['Year', 'Round', 'Team']})
 
 # Merge the table with itself to get the previous performance:
 merged_df = pd.merge(merged_df, table_host, left_on=['Year', 'Round_prev', 'Team1_href'],
@@ -87,12 +126,12 @@ merged_df = pd.merge(merged_df, table_guest, left_on=['Year', 'Round_prev', 'Tea
 
 # Creating new columns for the difference between the current and previous performance:
 merged_df['Matches_ratio_last_5_host'] = merged_df['Won_matches_table_host'] - \
-                                         merged_df['Won_matches_table_host_prev']- \
+                                         merged_df['Won_matches_table_host_prev'] - \
                                          merged_df['Lost_matches_table_host'] + \
                                          merged_df['Lost_matches_table_host_prev']
 merged_df['Matches_ratio_last_5_guest'] = merged_df['Won_matches_table_guest'] - \
-                                          merged_df['Won_matches_table_guest_prev']- \
-                                          merged_df['Lost_matches_table_guest']+ \
+                                          merged_df['Won_matches_table_guest_prev'] - \
+                                          merged_df['Lost_matches_table_guest'] + \
                                           merged_df['Lost_matches_table_guest_prev']
 
 # Removing unnecessary columns created during the merge:
@@ -104,14 +143,58 @@ merged_df = merged_df.rename(columns={'Round_x': 'Round'})
 merged_df = merged_df.drop(['Date', 'Capacity', 'Current_set_score_host', 'Current_set_score_guest',
                             'Final_point_score_host', 'Final_point_score_guest'], axis=1)
 
+# Creating season points to matches column:
+merged_df['Season_points_to_matches_host'] = merged_df['Season_points_table_host'] / merged_df[
+    'Played_matches_table_host']
+merged_df['Season_points_to_matches_guest'] = merged_df['Season_points_table_guest'] / merged_df[
+    'Played_matches_table_guest']
+
 # Save the dataframe to a csv file:
-merged_df.to_csv('Plusliga_data.csv', index=False)
+merged_df.to_csv('../Datasets/Plusliga_data.csv', index=False)
 
-# TODO:
-# 1. Add the last 5 matches results for each team DONE
-# 2. Add the recent form - last 5 receptions, last 5 serves, last 5 points - switched to running sum
-# 3. Add the maximal point difference in the set and match      DONE
-# 4. When the phase == playoff, change it to last round of the regular season DONE
-# 5. Review the variables and change them to running sums/averages etc. DONE
-# 6. Add the historical matches between two teams
+# Limit the columns to the ones that are needed for the model:
+model_data = merged_df[['MatchID',  # Identifying info
+                        'Winner',  # Dependent variable
+                        'Time_Category',  # Match information
+                        'Year',
+                        'Phase',
+                        'Round_original',
+                        'Spectators',
+                        'Relative_spectators',
+                        'Matches_ratio_last_5_host',  # Table info variables
+                        'Matches_ratio_last_5_guest',
+                        'Season_points_to_matches_host',
+                        'Season_points_to_matches_guest',
+                        'Current_position_table_host',
+                        'Current_position_table_guest',
+                        'Sets_ratio_table_host',
+                        'Sets_ratio_table_guest',
+                        'Points_ratio_table_host',
+                        'Points_ratio_table_guest',
+                        'Set_number',  # Current situation variables
+                        'Current_point_difference',
+                        'Current_set_difference',
+                        'Max_point_difference_throughout_set',  # Match stats variables
+                        'Min_point_difference_throughout_set',
+                        'Max_point_difference_throughout_match',
+                        'Min_point_difference_throughout_match',
+                        'Running_net_crossings_average',
+                        'Current_host_serve_effectiveness',
+                        'Current_guest_serve_effectiveness',
+                        'Current_host_positive_reception_ratio',
+                        'Current_guest_positive_reception_ratio',
+                        'Current_host_perfect_reception_ratio',
+                        'Current_guest_perfect_reception_ratio',
+                        'Current_host_negative_reception_ratio',
+                        'Current_guest_negative_reception_ratio',
+                        'Current_host_attack_accuracy',
+                        'Current_guest_attack_accuracy',
+                        'Current_host_attack_effectiveness',
+                        'Current_guest_attack_effectiveness',
+                        'Current_timeouts_host',  # Coach decisions variables
+                        'Current_timeouts_guest',
+                        'Current_challenges_host',
+                        'Current_challenges_guest']]
 
+# Save the dataframe to a csv file:
+model_data.to_csv('../Datasets/Plusliga_data_for_model.csv', index=False)
